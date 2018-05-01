dcff73b230a0b688175fbd3f1da56f8915b6365c
catcher92 <catcher92@163.com>
2018-05-01 21:10:25 +0800

    flux返回序列，也不会阻塞Controller线程

--- src/main/java/com/catcher92/demo/spring/controller/IndexController.java
+++ src/main/java/com/catcher92/demo/spring/controller/IndexController.java
@@ -1,11 +1,14 @@
 package com.catcher92.demo.spring.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 /**
  * @author catcher92
@@ -31,6 +34,21 @@ public class IndexController {
         Mono<String> result = Mono.fromSupplier(() -> sayHello(name));
         log.info("hello2 end");
         return result;
+    }
+
+    @RequestMapping(value = "/hello3", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> hello3(String name) {
+        log.info("hello3 start");
+        Flux<String> stringFlux = Flux.fromStream(IntStream.rangeClosed(1, 5).mapToObj(i -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "flux-" + name + "-" + i;
+        }));
+        log.info("hello3 end");
+        return stringFlux;
     }
 
     private String sayHello(String name) {
