1f539ac4e0fb0cd6b3b65ee6f8f13a128eeb1804
catcher92 <catcher92@163.com>
2018-05-01 21:06:23 +0800

    webflux非阻塞方式。mono是惰性求值，所以Controller线程不会被阻塞

--- src/main/java/com/catcher92/demo/spring/controller/IndexController.java
+++ src/main/java/com/catcher92/demo/spring/controller/IndexController.java
@@ -1,27 +1,44 @@
 package com.catcher92.demo.spring.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author catcher92
  * @date 2018/5/1
  */
 @RestController
+@Slf4j
 public class IndexController {
 
     @RequestMapping("/hello1")
     public String hello1(String name) {
-        return sayHello(name);
+        log.info("hello1 start");
+        // controller线程会被一直占用
+        String str = sayHello(name);
+        log.info("hello1 end");
+        return str;
     }
 
     @RequestMapping("/hello2")
     public Mono<String> hello2(String name) {
-        return Mono.just(sayHello(name));
+        log.info("hello2 start");
+        // stream是惰性求职controller线程不会被阻塞
+        Mono<String> result = Mono.fromSupplier(() -> sayHello(name));
+        log.info("hello2 end");
+        return result;
     }
 
     private String sayHello(String name) {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return String.format("Hello %s!", name);
     }
 }
