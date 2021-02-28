09da3aba97809f0a430ca848da10c71131f735b6
caoxuedong <catcher92@163.com>
2021-02-28 16:17:09 +0800

    Upgrade spring-boot version to 2.4.3.

--- src/test/java/com/catcher92/demo/spring/DemoApplicationTests.java
+++ src/test/java/com/catcher92/demo/spring/DemoApplicationTests.java
@@ -1,11 +1,11 @@
 package com.catcher92.demo.spring;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
 @SpringBootTest
 public class DemoApplicationTests {
 
